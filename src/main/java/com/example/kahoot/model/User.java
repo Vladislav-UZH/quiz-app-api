f8d9ef8ce337996f04a336b57a2adcd622084f32
random-words <153684721+random-words@users.noreply.github.com>
2024-10-05 13:56:23 +0300

    updated

--- src/main/java/com/example/kahoot/model/User.java
+++ src/main/java/com/example/kahoot/model/User.java
@@ -1,7 +1,10 @@
 package com.example.kahoot.model;
 
+import org.springframework.stereotype.Component;
+
 import java.util.UUID;
 
+@Component
 public class User {
     private UUID id;
     private String name;
