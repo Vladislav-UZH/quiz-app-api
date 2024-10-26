02f8d5a00b61f3c7f70af3df799d1be7e9df7ce1
random-words <153684721+random-words@users.noreply.github.com>
2024-10-27 01:10:00 +0300

    ce pizdessssss

---
+++ src/main/java/com/example/kahoot/enums/TokenValidity.java
@@ -0,0 +1,16 @@
+package com.example.kahoot.enums;
+
+public enum TokenValidity {
+    ACCESS_TOKEN_VALIDITY(5 * 60 * 1000), // 5 хвилин
+    REFRESH_TOKEN_VALIDITY(7 * 24 * 60 * 60 * 1000); // 7 днів
+
+    private final long validity;
+
+    TokenValidity(long validity) {
+        this.validity = validity;
+    }
+
+    public long getValidity() {
+        return validity;
+    }
+}
